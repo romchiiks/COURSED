a1a25382605ba17465ee4676572267da083ee416
Назаркин Роман <cvqsrsxl@gmail.com>
2024-04-09 21:34:45 +0300

    1 commit

---
+++ kyrsa4/Auth.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace kyrsa4
+{
+    /// <summary>
+    /// Interaction logic for Auth.xaml
+    /// </summary>
+    public partial class Auth : Page
+    {
+        public Auth()
+        {
+            InitializeComponent();
+        }
+
+        private void ButLogin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButReg_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ForgorPassword_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
